5c4dbfbb4d15c1dee546292061fc94133c3d171d
ngLiaXL <344212231@qq.com>
2017-03-31 14:45:11 +0800

    add bottom menu dialog

--- app/src/main/java/com/ldroid/kwei/common/ui/BaseActivity.java
+++ app/src/main/java/com/ldroid/kwei/common/ui/BaseActivity.java
@@ -5,8 +5,13 @@ package com.ldroid.kwei.common.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v4.app.FragmentActivity;
+import android.view.WindowManager;
+
+import com.ldroid.kwei.R;
 
 import butterknife.ButterKnife;
 
@@ -27,22 +32,37 @@ public abstract class BaseActivity extends FragmentActivity {
     }
 
     protected abstract void initPreparation();
-
     protected abstract void setContentView();
-
     protected abstract void initUI();
-
     protected abstract void initListener();
-
     protected abstract void initData();
-
-
     public void startAnimActivity(Intent intent) {
         startActivity(intent);
     }
-
     public void startAnimActivity(Class<?> cla) {
         startActivity(new Intent(this, cla));
+    }
+
+    public void setFloating(android.support.v7.widget.Toolbar toolbar, @StringRes int details) {
+        boolean isTablet = getResources().getBoolean(R.bool.isTablet);
+        if (isTablet) {
+            WindowManager.LayoutParams params = getWindow().getAttributes();
+            params.height = getResources().getDimensionPixelSize(R.dimen.floating_height);
+            params.width = getResources().getDimensionPixelSize(R.dimen.floating_width);
+            params.alpha = 1.0f;
+            params.dimAmount = 0.6f;
+            params.flags |= 2;
+            getWindow().setAttributes(params);
+
+            if (details != 0) {
+                toolbar.setTitle(details);
+            }
+            //toolbar.setNavigationIcon(R.drawable.ic_close);
+            setFinishOnTouchOutside(true);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
     }
 
 }
--- app/src/main/java/com/ldroid/kwei/module/main/MainActivity.java
+++ app/src/main/java/com/ldroid/kwei/module/main/MainActivity.java
@@ -1,72 +1,89 @@
 package com.ldroid.kwei.module.main;
 
 import android.content.Context;
+import android.view.View;
 
 import com.ldroid.kwei.R;
 import com.ldroid.kwei.common.trace.Timber;
 import com.ldroid.kwei.common.ui.BaseActivity;
+import com.ldroid.kwei.common.ui.lib.pop.BottomMenuDialog;
 
 import butterknife.OnClick;
 
 
-public class MainActivity extends BaseActivity implements  MainContract.View {
+public class MainActivity extends BaseActivity implements MainContract.View {
 
 
-	private MainPresenter mPresenter;
+    private MainPresenter mPresenter;
 
-	@Override
-	protected void initPreparation() {
-		mPresenter = new MainPresenter(this) ;
-	}
+    @Override
+    protected void initPreparation() {
+        mPresenter = new MainPresenter(this);
+    }
 
-	@Override
-	protected void setContentView() {
-		setContentView(R.layout.ac_main);
-	}
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.ac_main);
+    }
 
-	@Override
-	protected void initUI() {
-	}
+    @Override
+    protected void initUI() {
 
-	@Override
-	protected void initListener() {
+    }
 
-	}
+    @Override
+    protected void initListener() {
 
-	@Override
-	protected void initData() {
+    }
 
-	}
+    @Override
+    protected void initData() {
+
+    }
 
 
-	@OnClick(R.id.send_http)
-	public void onClickWeather(){
-		Timber.tag("MainAc");
-		Timber.d("aaaaaaaaaaaabbbbbbbbbcccccccc");
-	}
+    @OnClick(R.id.send_http)
+    public void onClickWeather() {
+        Timber.tag("MainAc");
+        Timber.d("aaaaaaaaaaaabbbbbbbbbcccccccc");
+        BottomMenuDialog dialog = new BottomMenuDialog.BottomMenuBuilder()
+                .addItem("拍照", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-	@Override
-	public void onRespLogin() {
+                    }
+                })
+                .addItem("相册中选择", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    }
+                })
+                .addItem("取消", null).build();
+        dialog.show(getSupportFragmentManager());
+    }
 
-	}
+    @Override
+    public void onRespLogin() {
 
-	@Override
-	public Context getContext() {
-		return null;
-	}
+    }
 
-	@Override
-	public void showLoading(String msg) {
+    @Override
+    public Context getContext() {
+        return null;
+    }
 
-	}
+    @Override
+    public void showLoading(String msg) {
 
-	@Override
-	public void dismissLoading() {
+    }
 
-	}
+    @Override
+    public void dismissLoading() {
 
-	@Override
-	public void onError(String msg) {
+    }
 
-	}
+    @Override
+    public void onError(String msg) {
+
+    }
 }
